9b1105c21f991e1cf6068de658bc52065e901412
Nadiia Bartoshyk <bart.nadiia@gmail.com>
2018-06-06 18:01:51 +0300

    Ошибка в файле Quote.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="flex justify-center bg-black">
+      <h1 class="flex justify-center w-1/2 text-white">M o v i e s</h1>
+      <p class="quote flex justify-center w-1/2 items-center text-white">"Каждый хочет быть Кэри Грантом. Даже я хотел бы им быть." (
+Кэри Грант)</p>
     </div>
+    <div id="nav">
+      <router-link to="/">Все фильмы</router-link> 
+      <router-link to="/about">Комедии</router-link>
+    </div>
+    <Carusel></Carusel>
+    <Quote></Quote>
     <router-view/>
   </div>
 </template>
 
+<script>
+import Carusel from '@/components/Carusel.vue'
+import Quote from '@/components/Quote.vue'
+
+export default {
+  name:'carusel',
+  name:'quote',
+  components:{
+    Carusel,
+    Quote
+  }
+}
+  
+
+
+</script>
+
+
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: 'Alegreya Sans SC', sans-serif;
+    font-family: 'Lobster', cursive;
+    font-family: 'Bad Script', cursive;
+    font-family: 'Cormorant SC', serif;
+    font-family: 'Cormorant Infant', serif;
+    font-family: 'Ledger', serif;
+    font-family: 'Jura', sans-serif;
+    font-family: 'Tenor Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+.quote{
+ font-style: italic;
 }
 </style>
 <style src="./assets/styles/main.css">
---
+++ src/components/Carusel.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="carusel h-64 bg-red-lightest">Карусель</div>
+</template>
+
+<script>
+export default {
+  name: 'carusel',
+}
+</script>
+
